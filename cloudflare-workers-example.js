7dbe48b49b0227e4013af77414abbeea56eafda0
Jason Gorman <mrjasongorman@gmail.com>
2021-04-02 15:40:01 +0100

    Update cloudflare-workers-example.js

--- cloudflare-workers-example.js
+++ cloudflare-workers-example.js
@@ -1,5 +1,5 @@
 blacklist = [ ]; // blacklisted origins
-whitelist = [ "http://localhost:8080", "https://myfeedrss.jasongorman.uk" ]; // whitelisted origins
+whitelist = [ "http://localhost:8080", "https://myfeed.jasongorman.uk" ]; // whitelisted origins
 
 const isListed = (uri, listing) => {
     let result = false;
@@ -15,56 +15,57 @@ const isListed = (uri, listing) => {
 
 addEventListener("fetch", async event => {
     event.respondWith((async function() {
-        const proxy_url = new URL(event.request.url);
-        const fetchOrigin = event.request.headers.get("Origin");
+            const proxy_url = new URL(event.request.url);
+            const fetchOrigin = event.request.headers.get("Origin");
 
-        // Check header origin
-        if ((!isListed(fetchOrigin, blacklist)) && (isListed(fetchOrigin, whitelist))) {
-            
-            // Get URL to consume
-            const fetch_url = decodeURIComponent(proxy_url.search.substr(1));
-            
-            if(fetch_url === '') {
+            // Check header origin
+            if ((!isListed(fetchOrigin, blacklist)) && (isListed(fetchOrigin, whitelist))) {
+                
+                // Get URL to consume
+                const fetch_url = decodeURIComponent(proxy_url.searchParams.get('s'));
+                
+                if(fetch_url === '') {
+                    return new Response(
+                        "Not Found",
+                        {
+                            status: 404,
+                            statusText: 'Not Found',
+                            headers: {
+                                "Content-Type": "text/plain"
+                            }
+                        }
+                    );
+                }
+
+                // Fetch and add CORS header
+                const response = await fetch(fetch_url);
+                let myHeaders = new Headers(response.headers);
+                myHeaders.set("Access-Control-Allow-Origin", fetchOrigin);
+
+                const body = await response.arrayBuffer();
+
+                const newHeaders = {
+                    headers: myHeaders,
+                    status: response.status,
+                    statusText: response.statusText
+                };
+
+                return new Response(body, newHeaders);
+
+            } else {
+
                 return new Response(
-                    "Not Found",
+                    "Forbidden",
                     {
-                        status: 404,
-                        statusText: 'Not Found',
+                        status: 403,
+                        statusText: 'Forbidden',
                         headers: {
                             "Content-Type": "text/plain"
                         }
                     }
                 );
+
             }
+        })());
 
-            // Fetch and add CORS header
-            const response = await fetch(fetch_url);
-            let myHeaders = new Headers(response.headers);
-            myHeaders.set("Access-Control-Allow-Origin", fetchOrigin);
-
-            const body = await response.arrayBuffer();
-
-            const newHeaders = {
-                headers: myHeaders,
-                status: response.status,
-                statusText: response.statusText
-            };
-
-            return new Response(body, newHeaders);
-
-        } else {
-
-            return new Response(
-                "Forbidden",
-                {
-                    status: 403,
-                    statusText: 'Forbidden',
-                    headers: {
-                        "Content-Type": "text/plain"
-                    }
-                }
-            );
-
-        }
-    })());
 });
